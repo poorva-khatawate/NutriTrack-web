a386cf6117714f61e761f404a0103d940a398a9f
poorva-khatawate <01fe21bcs302@kletech.ac.in>
2024-05-14 23:30:10 +0530

    responsive

--- src/components/Track-calorie/Cart.js
+++ src/components/Track-calorie/Cart.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useCart } from 'react-use-cart';
 import { toast } from 'react-hot-toast';
 import Navbar from '../Navbar/Navbar';
+import './Cart.css';
 const Cart = () => {
     const {
         isEmpty,
@@ -47,9 +48,9 @@ const Cart = () => {
                                         <td>{item.calories}</td>
                                         <td>Serves({item.quantity})</td>
                                         <td>
-                                            <button className="btn btn-info ms-2" onClick={() => updateItemQuantity(item.id, item.quantity - 1)}>-</button>
-                                            <button className="btn btn-info ms-2" onClick={() => updateItemQuantity(item.id, item.quantity + 1)}>+</button>
-                                            <button className="btn btn-danger ms-2" onClick={() => handleRemoveItem(item.id)}>Remove Items</button>
+                                            <button className="btn btn-info ms-2 remove-btn" onClick={() => updateItemQuantity(item.id, item.quantity - 1)}>-</button>
+                                            <button className="btn btn-info ms-2 remove-btn" onClick={() => updateItemQuantity(item.id, item.quantity + 1)}>+</button>
+                                            <button className="btn btn-danger ms-2 remove-btn" onClick={() => handleRemoveItem(item.id)}>Remove Items</button>
                                         </td>
                                     </tr>
                                 );
