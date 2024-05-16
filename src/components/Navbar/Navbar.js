66db7ecd7fb266b73df98aeb9a3e9fe8fe89e742
Ritwik993 <ritwik2025@gmail.com>
2024-05-17 00:13:59 +0530

    Navbar made responsive

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,33 +1,69 @@
-import React from 'react';
-import { Link} from 'react-router-dom';
-import './Navbar.css';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 import { RxHamburgerMenu } from "react-icons/rx";
+import cross_icon from "./cross_icon.png";
+import { HiXMark } from "react-icons/hi2";
 const Navbar = ({ toggleLoginForm, setToggleLoginForm }) => {
+  const [toggleNav, setToggleNav] = useState(false);
 
-// const handleLogOut = () => {
-//   signOut(auth).then(() => {
-//     // Sign-out successful.
-//     navigate("/");
-//   }).catch((error) => {
-//     // An error happened.
-//   });
-//   };
+  // const handleLogOut = () => {
+  //   signOut(auth).then(() => {
+  //     // Sign-out successful.
+  //     navigate("/");
+  //   }).catch((error) => {
+  //     // An error happened.
+  //   });
+  //   };
 
   return (
-    <div className='navbarr'>
-      <div className='nav-leftt'>
-        <h1 className='nav-logo'>NutriTrack</h1>
-        </div>
-        <div className='nav-search'>
-        <Link to="/" className='nav-links'>Home</Link>
-        <Link to="/about" className='nav-links'>About</Link>
+    <div className="navbarr">
+      <div className="nav-leftt">
+        <h1 className="nav-logo">NutriTrack</h1>
+      </div>
+      <div className="nav-search">
+        <Link to="/" className="nav-links ">
+          Home
+        </Link>
+        <Link to="/about" className="nav-links ">
+          About
+        </Link>
         {/* <Link to="/" className='nav-links' onClick={handleLogOut}>Logout</Link> */}
-        <p className='nav-links sign_btn' onClick={() => setToggleLoginForm(true)}>Sign in</p>
+        <p
+          className="nav-links sign_btn "
+          onClick={() => setToggleLoginForm(true)}
+        >
+          Sign in
+        </p>
+        <div className="hamburger">
+          <RxHamburgerMenu
+            className="hamburger_icon"
+            onClick={() => setToggleNav(true)}
+          />
+          {toggleNav && (
+            <div className="hamburger_nav">
+              <div className="home_res">
+                <Link to="/" className="nav-links1">
+                  Home
+                </Link>
+                {/* <img src={cross_icon} className="cross" alt="cross" onClick={()=>setToggleNav(false)}/> */}
+                <HiXMark className="cross" onClick={()=>setToggleNav(false)} />
+              </div>
+              <Link to="/about" className="nav-links1">
+                About
+              </Link>
+              <p
+                className="nav-links1 sign_btn"
+                onClick={() => setToggleLoginForm(true)}
+              >
+                Sign in
+              </p>
+            </div>
+          )}
         </div>
       </div>
+    </div>
   );
-}
+};
 
 export default Navbar;
-
-
